955a433c17d43568f523ac467951ec6ffbb649c5
Rajnish Chauhan <crajnish425@gmail.com>
2026-06-02 03:33:03 +0530

    update render url

--- ecommerce/src/api/axiosClient.jsx
+++ ecommerce/src/api/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://ecommerce-backend-ru9v.onrender.com',
+    baseURL: 'https://ecommerce-backend-cz73.onrender.com',
     withCredentials: true, // Crucial for security and sessions
     headers: {
         'Content-Type': 'application/json'
