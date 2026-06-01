f88c12d10afec9e53b2bf1dce437823e659efc66
Rajnish Chauhan <crajnish425@gmail.com>
2026-06-02 02:06:40 +0530

    add privacy and policy and render domain becoz render allow only 2 free custom domain

--- ecommerce/src/api/axiosClient.jsx
+++ ecommerce/src/api/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://ecommerce.backend.rajnishsystems.in/',
+    baseURL: 'https://ecommerce-backend-ru9v.onrender.com',
     withCredentials: true, // Crucial for security and sessions
     headers: {
         'Content-Type': 'application/json'
