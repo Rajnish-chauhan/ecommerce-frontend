7db4f71789b8744ea3eb7bec46beff7076a1a2a6
Rajnish Chauhan <crajnish425@gmail.com>
2026-06-02 11:17:53 +0530

    fix error

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -65,7 +65,7 @@ export default function Cart() {
 
             const options = {
                 // 🔥 REPLACE THIS WITH YOUR REAL RAZORPAY TEST KEY ID 🔥
-                key:rzp_test_StdeHZPBsoj0nM, 
+                key:"rzp_test_StdeHZPBsoj0nM", 
                 amount: totalAmount * 100, 
                 currency: "INR",
                 name: "SastaHai",
