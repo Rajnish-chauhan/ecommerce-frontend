ccd3323cfc597bcafaa38cf67e682abe04ee6c9b
Rajnish Chauhan <crajnish425@gmail.com>
2026-06-03 14:58:17 +0530

    update project and contact

--- ecommerce/src/pages/Cart.jsx
+++ ecommerce/src/pages/Cart.jsx
@@ -65,7 +65,7 @@ export default function Cart() {
 
             const options = {
                 // 🔥 REPLACE THIS WITH YOUR REAL RAZORPAY TEST KEY ID 🔥
-                key:`${VITE_RAZORPAY_KEY_ID}`, 
+                key:`${"VITE_RAZORPAY_KEY_ID"}`, 
                 amount: totalAmount * 100, 
                 currency: "INR",
                 name: "SastaHai",
